d4e79827ad94bc4475f0c0ead48b94362ca53e92
momonomono <momonomono1128@gmail.com>
2025-04-03 18:16:15 +0900

    sidebar.blade.php を追加しました

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.app')
+
+@section('title','トップページ')
+
+@section('content')
+    <div class="l-content u-mt-3">
+       
+        <div class="container">
+            <p class="sample">
+                <a href="/contact">サンプル依頼はこちら</a>
+            </p>
+            <ul>
+                <li><a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0GKTmW1G310S1p_Hi8rNQuv9dlzwTRubadHkWPpg4WnKBrQJtKZjiLPZ1KnkjoS3hSzwIPZtQP">東京</a></li>
+                <li><a href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ39l0la4rO8SLsOFv5LohTBJBcOt2tNoSyh7ku2EGUM_muHumLJIGYWYkSbPYDsV2anKchKr9vM">大阪</a></li>
+                <li><a href="https://calendar.app.google/GvPCmkf5W7BGu9RT">名古屋</a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
